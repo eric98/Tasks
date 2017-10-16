4547210ce941de936beeabb6a42e690c522a9c16
eric98 <ergare.17@gmail.com>
2017-10-16 21:23:21 +0200

    Initial version

---
+++ resources/assets/js/components/TasksComponent.vue
@@ -0,0 +1,122 @@
+<template>
+    <div id="root">
+        <ul>
+            <li v-for="task in filteredTasks" :class="{ completed: isCompleted(task) }" @dblclick="updateTask(task)">
+                <input type="text" v-model="newName" id="newName" v-if="task==editedTask"
+                       @keyup.enter="editTask(task)" @keyup.esc="cancelEdit(task)">
+                <div v-else>
+                    @{{task.name}}
+                    <i class="fa fa-pencil" aria-hidden="true" @click="updateTask(task)"></i>
+                    <i class="fa fa-times" aria-hidden="true" @click="deleteTask(task) = null"></i>
+                </div>
+            </li>
+        </ul>
+        Tasques pendents: @{{ pendingTasks }}
+        <br>
+        Nova Tasca a afegir: <input type="text" v-model="newTask" id="newTask" @keyup.enter="addTask">
+        <button id="add" @click="addTask">Afegir</button>
+
+        <h2>Filtres</h2>
+
+        <ul>
+            <li @click="show('all')" :class="{ active: this.filter === 'all' }">All</li>
+            <li @click="show('completed')" :class="{ active: this.filter === 'completed' }">Completed</li>
+            <li @click="show('pending')" :class="{ active: this.filter === 'pending' }">Pending</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+    li.completed {
+        text-decoration : line-through;
+    }
+
+    [v-cloak] { display: none; }
+
+    li.active {
+        background-color: blue;
+    }
+</style>
+
+<script>
+    var filters = {
+        all: function (tasks) {
+            return tasks
+        },
+        pending: function (tasks) {
+            return tasks.filter(function (task) {
+                return !task.completed
+            })
+        },
+        completed: function (tasks) {
+            return tasks.filter(function (task) {
+                return task.completed
+            })
+        }
+    }
+
+    const LOCAL_STORAGE_KEY = 'TASKS'
+
+    export default {
+        data() {
+            return {
+                editedTask: null,
+                filter: 'all',
+                newName: '',
+                newTask: '',
+                tasks: []
+            }
+        },
+        computed: {
+            // a computed getter
+            filteredTasks() {
+                return filters[this.filter](this.tasks)
+            },
+            pendingTasks() {
+                return filters['pending'](this.tasks).length
+            }
+        },
+        methods: {
+            show(filter) {
+                this.filter = filter
+            },
+            addTask() {
+                this.tasks.push({'name' : this.newTask, 'completed' : false})
+                this.newTask=''
+            },
+            isCompleted(task) {
+                return task.completed
+            },
+            deleteTask(task) {
+                this.tasks.splice(this.tasks.indexOf(task) , 1)
+            },
+            updateTask(task) {
+                this.newName = task.name
+                this.editedTask = task
+            },
+            editTask(task) {
+                this.editedTask.name=this.newName
+                this.editedTask = null
+            },
+            cancelEdit(task) {
+                this.editedTask = null
+            },
+            mounted() {
+                console.log('Inici')
+                //TODO Obtenir les tasques de la LocalStorage
+                //
+//                this.tasks = localStorage.getItem()
+//                this.newTasks = localStorage.setItem('NEW_TASK','Nova tasca')
+
+                this.tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]')
+
+//                JSON.parse Json -> Objecte -> Quan llegim de Local Storage
+//                 JSON.stringify()-> Objecte a Json -> Guardar a la base de dades
+
+                // json_encode() | json_decode() PHP!!!!
+            },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    Tasks
+@endsection
+
+
+@section('main-content')
+    <example-component></example-component>
+    <tasks></tasks>
+@endsection
