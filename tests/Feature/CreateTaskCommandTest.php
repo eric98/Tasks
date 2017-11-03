ab20ff7428ca9f0bfe52ebfe2bdbaa69958c4931
eric98 <ergare.17@gmail.com>
2017-11-03 16:20:47 +0100

    Sync

---
+++ tests/Feature/CreateTaskCommandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateTaskCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testItCreatesNewTask()
+    {
+        //1) Preparo
+
+        //2) Executo
+
+//        $this->artisan('route:list');
+        $this->artisan('task:create', ['name' => 'Comprar pa']);
+//        $this->artisan('task:create');
+
+        //3)Assert
+        // If you need result of console output
+        $resultAsText = Artisan::output();
+
+        //TODO Assert database Has?
+        $this->assertDatabaseHas('tasks',['name' => 'Comprar pa']);
+
+        //Receive "Task has been added to database succesfully."
+//        $this->assertTrue(str_contains($resultAsText, 'Task has been added to database succesfully'));
+        $this->assertContains('Task has been added to database succesfully',$resultAsText);
+    }
+}
