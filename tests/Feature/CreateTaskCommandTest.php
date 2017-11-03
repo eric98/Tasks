dfbdef7240f6e7eae1b34bb3160378e9636ae690
eric98 <ergare.17@gmail.com>
2017-11-03 17:01:31 +0100

    Sync

--- tests/Feature/CreateTaskCommandTest.php
+++ tests/Feature/CreateTaskCommandTest.php
@@ -53,4 +53,7 @@ class CreateTaskCommandTest extends TestCase
         $resultAsText = Artisan::output();
         $this->assertContains('Task has been added to database succesfully',$resultAsText);
     }
+
+    //TODO fer test per a delete (li passem el id)
+    //TODO fer test per a show (list)
 }
