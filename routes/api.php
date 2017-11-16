31daf466bcfa290146f6f83ae720ede08532a156
eric98 <ergare.17@gmail.com>
2017-11-16 16:55:51 +0100

    Sync

--- routes/api.php
+++ routes/api.php
@@ -18,17 +18,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'api','prefix' => 'v1', 'middleware' => ['throttle','bindings']], function () {
-    Route::group(['middleware' => 'auth:api'], function() {
+//    Route::group(['middleware' => 'auth:api'], function() {
         Route::get('tasks',                     'ApiTaskController@index');
         Route::get('tasks/{task}',              'ApiTaskController@show');
         Route::post('tasks',                    'ApiTaskController@store');
         Route::put('tasks/{task}',              'ApiTaskController@update');
         Route::delete('tasks/{task}',           'ApiTaskController@destroy');
 
-        Route::get('v1/users',                     'ApiUserController@index');
-        Route::get('v1/users/{task}',              'ApiUserController@show');
-        Route::post('v1/users',                    'ApiUserController@store');
-        Route::put('v1/users/{task}',              'ApiUserController@update');
-        Route::delete('v1/users/{task}',           'ApiUserController@destroy');
-    });
+        Route::get('users',                     'ApiUserController@index');
+        Route::get('users/{task}',              'ApiUserController@show');
+        Route::post('users',                    'ApiUserController@store');
+        Route::put('users/{task}',              'ApiUserController@update');
+        Route::delete('users/{task}',           'ApiUserController@destroy');
+//    });
 });
