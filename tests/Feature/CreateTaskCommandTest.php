5fcc0386db4a09fa795e64b14dcb5ea66d835c73
eric98 <ergare.17@gmail.com>
2017-12-04 21:32:43 +0100

    Sync

--- tests/Feature/CreateTaskCommandTest.php
+++ tests/Feature/CreateTaskCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use Mockery;
@@ -14,24 +15,29 @@ class CreateTaskCommandTest extends TestCase
     public function testItCreatesNewTask()
     {
         //1) Prepare
+        $user = factory(User::class)->create();
 
         //2) Execute
 
-        $this->artisan('task:create', ['name' => 'Comprar pa']);
+        $this->artisan('task:create',[
+            'name' => 'Comprar pa',
+            'user_id' => $user->id,
+        ]);
 
         //3)Assert
         // If you need result of console output
         $resultAsText = Artisan::output();
 
-        $this->assertDatabaseHas('tasks', ['name' => 'Comprar pa']);
+        $this->assertDatabaseHas('tasks', ['name' => 'Comprar pa', 'user_id' => $user->id]);
 
         //Receive "Task has been added to database succesfully."
-//        $this->assertTrue(str_contains($resultAsText, 'Task has been added to database succesfully'));
         $this->assertContains('Task has been added to database succesfully', $resultAsText);
     }
 
     public function testItAsksForATaskNameAndThenCreatesNewTask2()
     {
+        $user = factory(User::class)->create();
+
         //1) Prepare
         $command = Mockery::mock('App\Console\Commands\CreateTaskCommand[ask]');
 
@@ -40,12 +46,17 @@ class CreateTaskCommandTest extends TestCase
             ->with('Task name?')
             ->andReturn('Comprar llet');
 
+        $command->shouldReceive('ask')
+            ->once()
+            ->with('Task user_id?')
+            ->andReturn($user->id);
+
         $this->app['Illuminate\Contracts\Console\Kernel']->registerCommand($command);
 
         //2) Execute
         $this->artisan('task:create');
 
-        $this->assertDatabaseHas('tasks', ['name' => 'Comprar llet']);
+        $this->assertDatabaseHas('tasks', ['name' => 'Comprar llet','user_id' => $user->id]);
 
         //3) Assert
         $resultAsText = Artisan::output();
