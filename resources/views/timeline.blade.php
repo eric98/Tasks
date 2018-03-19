aef0033ed12e8c4bee5fd1aa59041b6f06f71bd3
eric98 <ergare.17@gmail.com>
2018-03-19 17:37:24 +0100

    Sync

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,10 +1,34 @@
 @extends('adminlte::layouts.app')
 
 @section('htmlheader_title')
-	HEADER_TITLE
+	Timeline Tasques
 @endsection
 
 
 @section('main-content')
-	CONTENT
+	<ul class="timeline">
+		@foreach($task_events as $event)
+		<li class="time-label">
+        <span class="bg-red">
+            10 Feb. 2014
+        </span>
+		</li>
+		<li>
+			<i class="fa fa-envelope bg-blue"></i>
+			<div class="timeline-item">
+				<span class="time"><i class="fa fa-clock-o"></i>{{$event->time}}</span>
+
+				<h3 class="timeline-header">Task name: <a href="/tasks_php/{{$event->id}}">{{ $event->task_name }}</a></h3>
+
+				<div class="timeline-body">
+					Task <b>{{ $event->task_name }}</b> was created at <b>{{ $event->created_at }}</b>, and is owned by <b>{{ $event->user_name }}</b></b>.
+				</div>
+
+				<div class="timeline-footer">
+					<a class="btn btn-primary btn-xs">...</a>
+				</div>
+			</div>
+		</li>
+		@endforeach
+	</ul>
 @endsection
