9d2440207e3670d8d449d3ca0c894148883b9b96
eric98 <ergare.17@gmail.com>
2018-03-19 18:16:44 +0100

    Sync

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -10,13 +10,13 @@
 		@foreach($task_events as $event)
 		<li class="time-label">
         <span class="bg-red">
-            10 Feb. 2014
+            {{date('j F, Y', strtotime($event->time))}}
         </span>
 		</li>
 		<li>
 			<i class="fa fa-envelope bg-blue"></i>
 			<div class="timeline-item">
-				<span class="time"><i class="fa fa-clock-o"></i>{{$event->time}}</span>
+				<span class="time"><i class="fa fa-clock-o"></i>{{date('G:i', strtotime($event->time))}}</span>
 
 				<h3 class="timeline-header">Task name: <a href="/tasks_php/{{$event->id}}">{{ $event->task_name }}</a></h3>
 
@@ -24,9 +24,9 @@
 					Task <b>{{ $event->task_name }}</b> was created at <b>{{ $event->created_at }}</b>, and is owned by <b>{{ $event->user_name }}</b></b>.
 				</div>
 
-				<div class="timeline-footer">
-					<a class="btn btn-primary btn-xs">...</a>
-				</div>
+				{{--<div class="timeline-footer">--}}
+					{{--<a class="btn btn-primary btn-xs">...</a>--}}
+				{{--</div>--}}
 			</div>
 		</li>
 		@endforeach
