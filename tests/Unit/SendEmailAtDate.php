a0bd6d14d27576b9da71dc200b4beedf24b2c400
eric98 <ergare.17@gmail.com>
2018-03-20 03:15:53 +0100

    Sync

--- tests/Unit/SendEmailAtDate.php
+++ tests/Unit/SendEmailAtDate.php
@@ -11,6 +11,6 @@ class SendEmailAtDate extends TestCase
      */
     public function send_email_if_is_correct_date()
     {
-        // No se com fer aquest test
+        // No se com fer aquest test per a comprovar que sol s'envia el correu si la data és correcta
     }
 }
