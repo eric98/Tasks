dd71e74f38ac7b035899cd4280755abdf2575936
eric98 <ergare.17@gmail.com>
2018-03-20 18:24:56 +0100

    Sync

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -20,10 +20,28 @@
                   </span>
                 </li>
             @endif
+
             <li>
-                <i class="fa fa-envelope bg-blue"></i>
+                @switch($event->type)
+                    @case('created')
+                        <i class="fa fa-pencil bg-green"></i>
+                    @break
+
+                    @case('updated')
+                        <i class="fa fa-pencil-square-o bg-yellow"></i>
+                    @break
+
+                    @case('deleted')
+                        <i class="fa fa-trash bg-red"></i>
+                    @break
+
+                    @default
+                        <i class="fa fa-envelope bg-blue"></i>
+                @endswitch
                 <div class="timeline-item">
-                    <span class="time"><i class="fa fa-clock-o"></i>{{date('G:i', strtotime($event->time))}}</span>
+                    <span class="time">
+                        <i class="fa fa-clock-o"></i>
+                        {{date('G:i', strtotime($event->time))}}</span>
 
                     <h3 class="timeline-header">{{$event->task_name}}, owned by {{$event->user_name}}</h3>
 
