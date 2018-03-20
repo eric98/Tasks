6344b6e9b5c3d8ede96656be9879730015244d65
eric98 <ergare.17@gmail.com>
2018-03-20 17:48:22 +0100

    Sync

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -7,14 +7,19 @@
 
 @section('main-content')
     <ul class="timeline">
+        <li class="time-label">
+            <span class="bg-red">
+                {{ $actualDate = date('j F, Y', strtotime($task_events->first()->time))}}
+            </span>
+        </li>
         @foreach($task_events as $event)
-            <li class="time-label">
-			    {{--@if(1==1)--}}
-                    <span class="bg-red">
-                        {{date('j F, Y', strtotime($event->time))}}
-                    </span>
-                {{--@endif--}}
-            </li>
+            @if(date('j F, Y', strtotime($event->time)) != $actualDate )
+                <li class="time-label">
+                  <span class="bg-red">
+                    {{ $actualDate = date('j F, Y', strtotime($event->time)) }}
+                  </span>
+                </li>
+            @endif
             <li>
                 <i class="fa fa-envelope bg-blue"></i>
                 <div class="timeline-item">
@@ -23,7 +28,8 @@
                     {{--<h3 class="timeline-header">Task name: <a href="/tasks_php/{{$event->id}}">{{ $event->task_name }}</a></h3>--}}
 
                     <div class="timeline-body">
-                        User <b>{{ $event->user_name }}</b> {{ $event->type }} task <b>{{ $event->task_name }}</b> at <b>{{date('F j, Y, g:i a', strtotime($event->time))}}</b>
+                        User <b>{{ $event->user_name }}</b> {{ $event->type }} task <b>{{ $event->task_name }}</b> at
+                        <b>{{date('F j, Y, g:i a', strtotime($event->time))}}</b>
                         {{--Task <b>{{ $event->task_name }}</b> was created <b>{{date('F j, Y, g:i a', strtotime($event->time))}}</b>, and is owned by <b>{{ $event->user_name }}</b>.--}}
                     </div>
 
